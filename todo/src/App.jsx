7e980a00ef8aa24faaec7e2924704db31974bb74
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-25 14:54:08 +0530

    Update App.jsx

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -11,15 +11,28 @@ function App(){
 return(
   < div className="card">
     <h1 className="heading">To-Do-App</h1>
-  <input className="content" type="text" value={task}onChange={(e)=>{
+  <input className="content" type="text" placeholder="Enter your task" value={task}onChange={(e)=>{
    settask( e.target.value)
   }}/>
   <button onClick={addtask} className = "submit">Submit</button>
   
   {
    todo.map((item, index)=><div className="covering">
-    <input type="text" className="content" value={item}/>
-    <button className="edit">edit</button>
+    <input type="text" className="input_field" value={item}/>
+    <button className="edit" onClick={()=>{
+      let int = document.getElementsByClassName("input_field")[0]
+      console.log(int)
+      let btn =document.querySelector(".edit")
+      if(btn.innerHTML === "Edit"){
+        btn.innerHTML = "Save"
+        int.removeAttribute("readonly","readonly")
+      }
+      else if(btn.innerHTML==="Save"){
+        btn.innerHTML = "Edit"
+        int.setAttribute("readonly","readonly")
+      }
+     
+          }}>Edit</button>
     <button 
     className="delete"
     onClick={()=>{
